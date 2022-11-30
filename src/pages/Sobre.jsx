8b18b9eb742e5e607d75621f5db1962dd3749068
Biganao <mfponte2006@gmail.com>
2022-11-30 00:31:39 -0300

    Adição de textos no sobre e mudança de  estilo no botão contato

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { Container } from 'react-bootstrap'
+import { Link } from "react-router-dom";
 
 import '../styles/style.css'
 
@@ -24,9 +25,13 @@ const Sobre = () => {
           </p>
           <h2 style={{marginTop: '4rem'}} id='info-tec'>Informações Técnicas</h2>
           <hr />
-          <h3 className='sobre-sub'>Histórico</h3>
+          <h3 className='sobre-sub'>A origem de Clio</h3>
           <p className='sobre'>
-            
+            O conceito inicial de Clio vem sido discutido desde maio de 2022, onde percebemos que muitos tem a vontade de ter o habito da leitura mas nunca encontram um livro desejado ou não tem ideia por onde começar. Assim que surge a ideia de criar uma plataforma/acervo para facilitar a busca e encontra de livros com o foco principalmente na literatura nacional pois também foi notado que poucos conhecem as obras literarias do Brasil e buscamos diminuir essa distancia entre leitor e livro.
+          </p>
+          <h3 className='sobre-sub'> Estrutura de negocio</h3>
+          <p className='sobre'>
+            Clio tem como base em sua estrutura um negocio 100% online com serviços realizados em uma plataforma/acervo digital de livros que possa ser facilmente encontrado na internet, além disso Clio busca sempre atender os pedidos e requisões dos nosso clientes por isso temos uma equipe de comunicações em prontidão para lhe dar o devido suporte e ajuda com oque precisar. Caso deseje entrar em contato com conosco clique <Link to='/contato'> aqui </Link>
           </p>
         </div>
       </Container>
