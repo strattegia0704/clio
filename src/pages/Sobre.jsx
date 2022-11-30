1a98a66c712748563149c59d421573658caf9fc1
Joisson Rocha <strattegiadev@gmail.com>
2022-11-30 01:11:15 +0000

    up to date: about page

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -14,13 +14,19 @@ const Sobre = () => {
         <div>
           <h2>Boas vindas à Clio!</h2>
           <hr />
-          <h3>Quem nós somos?</h3>
-          <p>
+          <h3 className='sobre-sub'>Quem nós somos?</h3>
+          <p className='sobre'>
             A Clio é um acervo digital de livros nacionais, onde está disponibilizado uma biblioteca em que os usuários poderão encontrar a sinopse, as informações gerais e <em>possíveis locais de compra</em> de determinada edição.
           </p>
-          <h3>Atividade empenhada</h3>
-          <p>
+          <h3 className='sobre-sub'>Atividade empenhada</h3>
+          <p className='sobre'>
             Nosso intuito com a proposta é facilitar a disseminação do conteúdo nacional de literatura, trazendo uma melhor distribuição e um acesso mais amplo, levando em consideração a escala dos meios digitais nos dias de hoje.
+          </p>
+          <h2 style={{marginTop: '4rem'}} id='info-tec'>Informações Técnicas</h2>
+          <hr />
+          <h3 className='sobre-sub'>Histórico</h3>
+          <p className='sobre'>
+            
           </p>
         </div>
       </Container>
