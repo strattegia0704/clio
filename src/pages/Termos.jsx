1f8e579a66a0c53074093ccc864a6f819c744cfb
Biganao <mfponte2006@gmail.com>
2023-01-06 00:51:44 -0300

    Styling: reponsive and header
    fixed geral responsive;
    texts align in mobile changes;
    header style change;
    login feature removed;

--- src/pages/Termos.jsx
+++ src/pages/Termos.jsx
@@ -14,45 +14,45 @@ const Termos = () => {
                                 <div>
                                         <h2><span>1. Termos</span></h2>
                                         <hr />
-                                        <p><span>Ao acessar ao site <a href="https://clio.vercel.app">Clio</a>, concorda em
+                                        <p className='sobre'><span>Ao acessar ao site <a href="https://clio.vercel.app">Clio</a>, concorda em
                                                 cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis ​​e concorda que é responsável pelo
                                                 cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses termos, está proibido de
                                                 usar ou acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos autorais e
                                                 marcas comerciais aplicáveis.</span></p>
                                         <h2><span>2. Uso de Licença</span></h2>
                                         <hr />
-                                        <p><span>É concedida permissão para baixar temporariamente uma cópia dos materiais
+                                        <p className='sobre'><span>É concedida permissão para baixar temporariamente uma cópia dos materiais
                                                 (informações ou software) no site Clio , apenas para visualização transitória pessoal e não comercial. Esta é a
                                                 concessão de uma licença, não uma transferência de título e, sob esta licença, você não pode:&nbsp;</span></p>
                                         <ol>
-                                                <li><span>modificar ou copiar os materiais;&nbsp;</span></li>
-                                                <li><span>usar os materiais para qualquer finalidade comercial ou para exibição
+                                                <li className='li-marc'><span>modificar ou copiar os materiais;&nbsp;</span></li>
+                                                <li className='li-marc'><span>usar os materiais para qualquer finalidade comercial ou para exibição
                                                         pública (comercial ou não comercial);&nbsp;</span></li>
-                                                <li><span>tentar descompilar ou fazer engenharia reversa de qualquer software
+                                                <li className='li-marc'><span>tentar descompilar ou fazer engenharia reversa de qualquer software
                                                         contido no site Clio;&nbsp;</span></li>
-                                                <li><span>remover quaisquer direitos autorais ou outras notações de propriedade dos
+                                                <li className='li-marc'><span>remover quaisquer direitos autorais ou outras notações de propriedade dos
                                                         materiais; ou&nbsp;</span></li>
-                                                <li><span>transferir os materiais para outra pessoa ou 'espelhe' os materiais em
+                                                <li className='li-marc'><span>transferir os materiais para outra pessoa ou 'espelhe' os materiais em
                                                         qualquer outro servidor.</span></li>
                                         </ol>
-                                        <p><span>Esta licença será automaticamente rescindida se você violar alguma dessas
+                                        <p className='sobre'><span>Esta licença será automaticamente rescindida se você violar alguma dessas
                                                 restrições e poderá ser rescindida por Clio a qualquer momento. Ao encerrar a visualização desses materiais ou
                                                 após o término desta licença, você deve apagar todos os materiais baixados em sua posse, seja em formato
                                                 eletrónico ou impresso.</span></p>
                                         <h2><span>3. Isenção de responsabilidade</span></h2>
                                         <hr />
                                         <ol>
-                                                <li><span>Os materiais no site da Clio são fornecidos 'como estão'. A Clio não oferece
+                                                <li className='li-marc'><span>Os materiais no site da Clio são fornecidos 'como estão'. A Clio não oferece
                                                         garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as outras garantias, incluindo,
                                                         sem limitação, garantias implícitas ou condições de comercialização, adequação a um fim específico ou não
                                                         violação de propriedade intelectual ou outra violação de direitos.</span></li>
-                                                <li><span>Além disso, a Clio não garante ou faz qualquer representação relativa à
+                                                <li className='li-marc'><span>Além disso, a Clio não garante ou faz qualquer representação relativa à
                                                         precisão, aos resultados prováveis ​​ou à confiabilidade do uso dos materiais em seu site ou de outra forma
                                                         relacionado a esses materiais ou em sites vinculados a este site.</span></li>
                                         </ol>
                                         <h2><span>4. Limitações</span></h2>
                                         <hr />
-                                        <p><span>Em nenhum caso a Clio ou seus fornecedores serão responsáveis ​​por quaisquer
+                                        <p className='sobre'><span>Em nenhum caso a Clio ou seus fornecedores serão responsáveis ​​por quaisquer
                                                 danos (incluindo, sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos negócios)
                                                 decorrentes do uso ou da incapacidade de usar os materiais em Clio, mesmo que Clio ou um representante
                                                 autorizado da Clio tenha sido notificado oralmente ou por escrito da possibilidade de tais danos. Como algumas
@@ -66,7 +66,7 @@ const Termos = () => {
                                                 entanto, a Clio não se compromete a atualizar os materiais.</span></p>
                                         <h2><span>6. Links</span></h2>
                                         <hr />
-                                        <p><span>A Clio não analisou todos os sites vinculados ao seu site e não é responsável
+                                        <p className='sobre'><span>A Clio não analisou todos os sites vinculados ao seu site e não é responsável
                                                 pelo conteúdo de nenhum site vinculado. A inclusão de qualquer link não implica endosso por Clio do site. O uso
                                                 de qualquer site vinculado é por conta e risco do usuário.</span></p>
                                         <hr />
