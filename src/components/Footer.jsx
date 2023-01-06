731512b64ed8bef962a854e22d88abd15ec33ab7
Biganao <mfponte2006@gmail.com>
2023-01-06 01:28:05 -0300

    Styling: footer
    Removed members section in footer;
    Centralized footer infomatins

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,29 +18,6 @@ const Footer = () => {
                             encontrados os títulos que você deseja!
                         </p>
                     </div>
-                    <div className="col-6 col-md-3 alunos">
-                        <h6> Membros <span className="f-mob"> do Grupo</span></h6>
-                        <ul className="footer-links">
-                            <li>
-                                <a style={{fontWeight: 'bolder',}}>Turma: 2º D</a>
-                            </li>
-                            <li>
-                                <a> Andressa Hitschelly</a>
-                            </li>
-                            <li>
-                                <a> Gabriel Almeida </a>
-                            </li>
-                            <li>
-                                <a> Iuri Cavalcante </a>
-                            </li>
-                            <li>
-                                <a> Joisson Rocha </a>
-                            </li>
-                            <li>
-                                <a> Matheus Filipe </a>
-                            </li>
-                        </ul>
-                    </div>
                     <div className="col-6 col-md-3">
                         <h6 id="cont"> Informações Adcionais </h6>
                         <ul className="footer-links">
